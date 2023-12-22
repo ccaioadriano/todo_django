822692c2dd8ff4382d6884dd3b5d48a0df80ad4c
Caio Adriano <caiobhadriano@gmail.com>
2023-12-21 22:46:58 -0300

    [Feat]: Creating migrations

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True, null=False ,blank=False, unique=False)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateTimeField(null=False, blank=False)
+    finished_at = models.DateTimeField(null=True)
